bb998d25730fff358a0b3308a43de03f1ac9ef09
Rakesh <mondalrakesh461@gmail.com>
2025-04-22 14:46:20 +0530

    styles added

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log(window);
+console.log("Script loaded");
+
+const cardSection = document.querySelector('.cardSection');
+const addBtn = document.getElementById('addBtn');
+
+addBtn.addEventListener('click', () => {
+  const card = document.createElement('div');
+  card.className = 'card';
+  card.innerHTML = `
+    <p>This is a card</p>
+    <button class="removeBtn">Remove</button>
+  `;
+  
+  // Attach event listener for remove button
+  card.querySelector('.removeBtn').addEventListener('click', () => {
+    card.remove();
+  });
+
+  cardSection.appendChild(card);
+});
